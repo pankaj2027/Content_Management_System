41085a7e4105077dbefec0438cf0126c57d240b2
pankaj chaudhary <pankaj@trisysyit.com>
2021-03-20 10:22:57 +0530

    created login Api for accessing  userToken by username and password.

--- content_management/cms/urls.py
+++ content_management/cms/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path,include
 from rest_framework import routers
 from cms.views import AuthorRegistration
-# from rest_framework.authtoken import views as authviews
+from rest_framework.authtoken import views as authviews
 
 
 router = routers.DefaultRouter()
@@ -9,5 +9,6 @@ router = routers.DefaultRouter()
 router.register('author_registration', AuthorRegistration, basename='author_registration')  
            
 urlpatterns = [
-    path('', include(router.urls)),  
+    path('', include(router.urls)),
+    path('login/',authviews.obtain_auth_token)  
 ]
